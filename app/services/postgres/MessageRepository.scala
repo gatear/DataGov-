364e90adbcee170d303eec8dbb824bbcad091935
gatear <razvangabrielgatea@gmail.com>
2017-12-22 22:24:27 +0200

    Added REST endpoint '/location', it returns nearby crimes based on input location.

--- app/services/postgres/MessageRepository.scala
+++ app/services/postgres/MessageRepository.scala
@@ -9,25 +9,31 @@ import scala.reflect.ClassTag
 
 
 class MessageRepository(pool :Connection) {
+import MessageRepository._
 
-  case class Row(data :String*)
 
-  case class Model (data :String, data2: String){
-
-  }
 
   def listDistribution ():Future[IndexedSeq[Seq[String]]] = {
 
 
-    val futureResult : Future[QueryResult] = pool.sendQuery(MessageRepository.SelectCrimeDistribution)
+    val futureResult : Future[QueryResult] = pool.sendQuery(SelectCrimeDistribution)
 
     val result: Future[IndexedSeq[Seq[String]]] = futureResult.map((qResult: QueryResult) => qResult.rows
-                                                                   .get
+                                                                   .get     //todo let percentage be float and use tuples directly!!!
                                                                    .map {case row => Seq( row("type").asInstanceOf[String], row("percentage").asInstanceOf[BigDecimal].floatValue().toString ) })
 
     result
   }
 
+  import models.Location
+  def checkLocation (location: Location): Future[IndexedSeq[(String, String)]] ={
+
+
+    val futureResult : Future[QueryResult] = pool.sendQuery(SelectCrimesNearLocation.replace("$1",location.toString))
+    val result = futureResult.map( qResult => qResult.rows match { case Some(resultSet: ResultSet) => resultSet.collect{case row =>row("Primary Type").toString -> row("distance").toString}})
+
+    result
+  }
 
 
   def selectDistribution(): Future[IndexedSeq[IndexedSeq[String]]] = {
@@ -39,10 +45,11 @@ class MessageRepository(pool :Connection) {
   }
 }
 object MessageRepository {
+  val SelectCrimesNearLocation = "SELECT \"Primary Type\", MIN(st_distance_sphere( st_centroid(geom), st_geomfromtext($1) )) distance\n  FROM crimes_2001_to_2017_chicago\n WHERE year > 2015\n GROUP BY \"Primary Type\"\nORDER BY distance"
 
-  val SelectCrimeDistribution = "(SELECT crimes.type, ROUND( ((CAST(crimes.cases AS NUMERIC) * 100)/ (SELECT COUNT(*) FROM crimes_2001_to_2016) ),4)  AS Percentage FROM " +
-    "(SELECT DISTINCT  crimes_2001_to_2016.\"Primary Type\" AS type, COUNT(crimes_2001_to_2016.\"Primary Type\") AS Cases " +
-    "FROM crimes_2001_to_2016 " +
-    "GROUP BY crimes_2001_to_2016.\"Primary Type\") AS  crimes) " +
+  val SelectCrimeDistribution = "(SELECT crimes.type, ROUND( ((CAST(crimes.cases AS NUMERIC) * 100)/ (SELECT COUNT(*) FROM crimes_2001_to_2017_chicago) ),4)  AS Percentage FROM " +
+    "(SELECT DISTINCT  crimes_2001_to_2017_chicago.\"Primary Type\" AS type, COUNT(crimes_2001_to_2017_chicago.\"Primary Type\") AS Cases " +
+    "FROM crimes_2001_to_2017_chicago " +
+    "GROUP BY crimes_2001_to_2017_chicago.\"Primary Type\") AS  crimes) " +
     "ORDER BY Percentage DESC"
 }
